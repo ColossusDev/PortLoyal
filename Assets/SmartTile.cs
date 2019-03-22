90084e305056de813da75f12cd5c0cae2fdac097
ColossusDev <maltekleimann@gmail.com>
2019-03-22 07:45:09 +0100

    Starting with SmartTile

---
+++ Assets/SmartTile.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartTile : MonoBehaviour
+{
+    public int[,] islandMap = null;
+    public int x;
+    public int y;
+
+    [SerializeField] Sprite UpRight;
+    [SerializeField] Sprite UpLeft;
+    [SerializeField] Sprite DownRight;
+    [SerializeField] Sprite DownLeft;
+
+    [SerializeField] Sprite LeftRightUp;
+    [SerializeField] Sprite LeftRightDown;
+    [SerializeField] Sprite UpDownLeft;
+    [SerializeField] Sprite UpDownRight;
+
+    [SerializeField] Sprite Left;
+    [SerializeField] Sprite Up;
+    [SerializeField] Sprite Right;
+    [SerializeField] Sprite Down;
+
+    [SerializeField] Sprite LeftRight;
+    [SerializeField] Sprite UpDown;
+
+    [SerializeField] Sprite LeftUpRightDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
